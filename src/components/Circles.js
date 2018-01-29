48de1aa2c6113de7e1c5dab781bd84da560762b5
github username <github email address>
2018-01-29 16:40:14 -0500

    Done.

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -5,7 +5,7 @@ class Circles extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
       circles: [
         { color: 'red' },
@@ -26,7 +26,7 @@ class Circles extends React.Component {
   }
 
   render() {
-    const renderCircles = this.state.circles.map((circle, index) => 
+    const renderCircles = this.state.circles.map((circle, index) =>
       <Circle color={circle.color} key={index} /> 
     );
 
